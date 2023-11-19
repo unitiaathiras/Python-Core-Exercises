29ac68dda30acd7bdbbebdb4f015a3e2d073e8af
ATHIRA <127323277+unitiaathiras@users.noreply.github.com>
2023-11-19 22:37:59 +0530

    Add files via upload

---
+++ Anagram.py
@@ -0,0 +1,46 @@
+'''Write a program that checks if two given strings are anagrams (contain the same characters
+with the same frequencies)'''
+
+string1 = input("enter string 1 ")
+string2 = input("enter string 2 ")
+
+if string1 == string2:
+    print("anagram")
+else:
+    print("not anagram")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(id(string1))
+# print(id(string2))
+
+# print(string2 is string1)
+
+# if string1 is string2:
+#     print("anagram")
+# else:
+#     print("not anagram")
+# a=10
+# b=10
+# print(a is b)
+
+# a="amma"
+# b="amma"
+# print(a is b)
+
+# if a is b:
+#     print("qwerty")
