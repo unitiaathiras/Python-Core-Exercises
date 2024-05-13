6e8ae926a92e4ce19f29e5febceac699a4b2d237
unitiaathira <athirashibuathi13@gmail.com>
2024-05-13 21:50:08 +0530

    operators in python

---
+++ practice2.py
@@ -0,0 +1,10 @@
+num1 = 21
+num2 = 10
+div = num1 / num2
+print("division:", div)
+rem = num1 % num2 
+print("remainder:", rem)
+exp = 2**3
+print("exponent", exp)
+floor = num1 // num2 # 21/10 = 2.1  here 2 is the floor value 
+print(floor)
---
+++ practice3.py
@@ -0,0 +1,8 @@
+# In Python same memory location for same value
+num1 = 10
+num2 = 10
+print(id(num1), id(num2))
+
+a = 25
+a//=4
+print(a) # in floor division we get the decimal portion after division.
